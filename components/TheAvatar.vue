168a67ac22e8394ab11e286f724b36c68abaeca5
dar11335469 <dar11335469@gmail.com>
2025-09-18 14:25:22 +0200

    Fix path to avatar image

--- components/TheAvatar.vue
+++ components/TheAvatar.vue
@@ -3,7 +3,7 @@
     class="max-w-[320px] max-h-[320px] max-md:max-w-[220px] max-md:max-h-[220px] w-full h-full border-2 border-white rounded-[50%] overflow-hidden flex shadow-2xl items-center justify-center"
   >
     <img
-      :src="`/img/avatar.jpg`"
+      src="/img/avatar.jpg"
       alt="Profile"
       class="w-full h-full block object-cover object-center"
     />
